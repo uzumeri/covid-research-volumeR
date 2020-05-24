b59156d4f157728d7e8c9160cd8c40500c1ca7e1
Vic Uzumeri <vic.uzumeri@ipov.net>
2020-05-24 14:33:08 -0400

    generated covid pub graph

--- articlestatistics.R
+++ articlestatistics.R
@@ -1,10 +1,13 @@
 library(tidyverse)
 setwd("~/github/covid-research-volumeR")
-rs <- read_excel("data/All_Articles_Excel_Reduced.xlsx") 
+rs <- read_excel("data/All_Articles_Excel_Reduced_Cleaned.xlsx") 
 
 research <- rs %>% select(`Date Added`, Author, Title, Year, `Journal/Publisher`, Language, Keywords) %>% 
   rename(date = `Date Added`, journal = `Journal/Publisher`) %>%
-  mutate(preprint = if_else(str_detect(journal,"Rxiv"),'Preprint','Reviewed'))
+  mutate(status = if_else(str_detect(journal,"Rxiv"),'Preprint','Reviewed'))
+
+research$date <- str_replace(research$date, " EDT ", " ")
+research$date <- as.Date(research$date, format = "%a %b %d %T %Y")
 
 datecount <- research %>% group_by(date) %>% 
   summarize(n()) %>% 
@@ -15,7 +18,7 @@ publications <- research %>% group_by(journal) %>%
   summarize(n()) %>% rename(count = 'n()') %>% 
   filter(count > 5) %>% arrange(journal) 
 
-preprints <- research %>% group_by(date, preprint) %>% 
+preprints <- research %>% group_by(date, status) %>% 
   summarize(n()) %>% rename(count = 'n()') %>% 
   filter(count > 5) %>% 
   filter(date > '2020-03-30')
@@ -23,5 +26,8 @@ preprints <- research %>% group_by(date, preprint) %>%
 g <- ggplot(datecount, aes(x=date, y=count)) + geom_point() + geom_smooth()
 g
 
-p <- ggplot(preprints, aes(x=date, y=count, color=preprint)) + geom_point() + geom_path()
+p <- ggplot(preprints, aes(x=date, y=count, color=status)) + 
+      geom_point() + geom_path() + geom_smooth() +
+      xlab("Database Entry Date") + ylab("Number of Publications Per Day") +
+      ggtitle("COVID Research Publication Rate (Worldwide)")
 p
