bacd39719d5d32ab0ee38430af2eb75b55ce576c
Vic Uzumeri <vic.uzumeri@ipov.net>
2020-05-24 18:30:28 -0400

    update

--- articlestatistics.R
+++ articlestatistics.R
@@ -5,6 +5,7 @@ rs <- read_excel("data/All_Articles_Excel_Reduced_Cleaned.xlsx")
 research <- rs %>% select(`Date Added`, Author, Title, Year, `Journal/Publisher`, Language, Keywords) %>% 
   rename(date = `Date Added`, journal = `Journal/Publisher`) %>%
   mutate(status = if_else(str_detect(journal,"Rxiv"),'Preprint','Regular'))
+rm(rs)
 
 research$date <- str_replace(research$date, " EDT ", " ")
 research$date <- as.Date(research$date, format = "%a %b %d %T %Y")
@@ -18,7 +19,7 @@ publications <- research %>% group_by(journal) %>%
   summarize(n()) %>% rename(count = 'n()') %>% 
   filter(count > 5) %>% arrange(journal) 
 
-preprints <- research %>% group_by(date, status) %>% 
+status <- research %>% group_by(date, status) %>% 
   summarize(n()) %>% rename(count = 'n()') %>% 
   filter(count > 5) %>% 
   filter(date > '2020-03-30')
@@ -27,10 +28,7 @@ languages <- research %>% group_by(Language) %>%
   summarize(n()) %>% rename(count = 'n()') %>% 
   filter(count > 5)
 
-g <- ggplot(datecount, aes(x=date, y=count)) + geom_point() + geom_smooth()
-g
-
-p <- ggplot(preprints, aes(x=date, y=count, color=status)) + 
+p <- ggplot(status, aes(x=date, y=count, color=status)) + 
       geom_point() + geom_smooth() +
       xlab("Database Entry Date") + ylab("Number of Publications Per Day") +
       ggtitle("COVID Research Publication Rate (Worldwide)")
